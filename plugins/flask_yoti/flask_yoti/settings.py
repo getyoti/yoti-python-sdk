f23a2dcbfdf62960ec9e9c8f2325fc1388917b65
anatoliis <anatolii@fastmail.com>
2016-11-04 16:19:03 +0200

    Flask plugin init commit

---
+++ plugins/flask_yoti/flask_yoti/settings.py
@@ -0,0 +1,28 @@
+from os import environ
+from flask import current_app
+
+
+def get_default_value(name):
+    return {
+        'YOTI_APPLICATION_ID': environ.get('YOTI_APPLICATION_ID'),
+        'YOTI_CLIENT_SDK_ID': environ.get('YOTI_CLIENT_SDK_ID'),
+        'YOTI_KEY_FILE_PATH': environ.get('YOTI_KEY_FILE_PATH'),
+        'YOTI_VERIFICATION_KEY': environ.get('YOTI_VERIFICATION_KEY', ''),
+        'YOTI_REDIRECT_TO': 'flask_yoti.profile',
+        'YOTI_LOGIN_VIEW': 'flask_yoti.login',
+        'YOTI_LOGIN_BUTTON_LABEL': 'Log in with Yoti'
+    }.get(name)
+
+
+def get_config_value(name):
+    try:
+        config = current_app.config
+        parameter = config.get(name, get_default_value(name))
+    except RuntimeError:
+        parameter = get_default_value(name)
+
+    if parameter is None:
+        raise RuntimeError(
+            'Required parameter "{0}" is not configured'.format(name)
+        )
+    return parameter
