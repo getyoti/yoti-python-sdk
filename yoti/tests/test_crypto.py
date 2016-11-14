9bda56eb9f22d09520ce08872e2c9e6227ec9463
anatoliis <anatolii@fastmail.com>
2016-11-14 13:50:17 +0200

    Fixes tests

--- yoti/tests/test_crypto.py
+++ yoti/tests/test_crypto.py
@@ -16,8 +16,9 @@ def test_decrypting_an_invalid_toke__should_not_be_allowed(invalid_token, crypto
         crypto.decrypt_token(invalid_token)
 
 
-def test_given_proper_encrypted_token__decrypting_should_yield_decrypted_token(encrypted_request_token, crypto):
-    expected_token = 'NpdmVVGC-8b1e5780-6073-47d6-8d54-66ddcb34041b-ec7d01d4-fb9f-4abb-97ad-5bd2ad5818f1'
+def test_given_proper_encrypted_token__decrypting_should_yield_decrypted_token(
+        encrypted_request_token, decrypted_request_token, crypto):
+    expected_token = decrypted_request_token
     decrypted_token = crypto.decrypt_token(encrypted_request_token).decode('utf-8')
     assert decrypted_token == expected_token
 
