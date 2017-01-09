3da57c6a414b8ccbdc8c8edbb2a3b2464d5e8dec
Omari Rodney <omari.rodney@yoti.com>
2017-01-09 10:31:07 +0000

    updated version number and licensing terms

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-VERSION = '0.1.2'
+VERSION = '1.0.0'
 long_description = 'This package contains the tools you need to quickly ' \
                    'integrate your Python back-end with Yoti, so that your ' \
                    'users can share their identity details with your ' \
@@ -11,20 +11,20 @@ setup(
     name='yoti',
     version=VERSION,
     packages=find_packages(),
-    license='MIT',
+    license='OTHER',
     description='Yoti Python SDK for back-end integration.',
     long_description=long_description,
-    url='https://github.com/lampkicking/yoti-sdk-server-python',
-    author='',
-    author_email='',
+    url='https://github.com/getyoti/python',
+    author='Yoti',
+    author_email='tech@yoti.com',
     install_requires=['cryptography>=1.4', 'protobuf>=3.0.0',
                       'requests>=2.0.0', 'future>=0.11.0'],
     extras_require={
         'examples': ['Django>=1.9', 'Flask>=0.10'],
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'License :: Other/Proprietary License',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
