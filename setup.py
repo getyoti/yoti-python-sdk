515774fde3dc411df8555f092474ab41a2638898
Edward Harrod <echarrod@users.noreply.github.com>
2017-10-04 17:29:20 +0100

    [SDK-39]: Changed to version 2.0.0, after addition of X-SDK headers, and added keywords. Similar to the .NET version, I decided to be consistent with that - even though we've changed the name of the package, there is a smaller chance of confusion if we bump the version to 2.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-VERSION = '1.2.0'
+VERSION = '2.0.0'
 long_description = 'This package contains the tools you need to quickly ' \
                    'integrate your Python back-end with Yoti, so that your ' \
                    'users can share their identity details with your ' \
@@ -38,5 +38,5 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    keywords='',
+    keywords='2FA multifactor authentication verification identity login register verify 2Factor',
 )
