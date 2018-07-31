280e6a4b77f573e6a2dd8739dbb345449132e024
Edward Harrod <echarrod@users.noreply.github.com>
2018-07-31 11:46:07 +0100

    SDK-350: Add source & verifiers filter

--- yoti_python_sdk/attribute.py
+++ yoti_python_sdk/attribute.py
@@ -1,3 +1,6 @@
+from yoti_python_sdk import config
+
+
 class attribute:
     name = ""
     value = ""
@@ -16,3 +19,9 @@ class attribute:
 
     def get_anchors(self):
         return self.anchors
+
+    def get_sources(self):
+        return list(filter(lambda a: a.anchor_type == config.ANCHOR_SOURCE, self.anchors))
+
+    def get_verifiers(self):
+        return list(filter(lambda a: a.anchor_type == config.ANCHOR_VERIFIER, self.anchors))
--- yoti_python_sdk/tests/test_anchor.py
+++ yoti_python_sdk/tests/test_anchor.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
-import binascii
 import datetime
-import io
-from os.path import abspath, dirname, join
 
-from yoti_python_sdk import anchor, config
-from yoti_python_sdk.protobuf.v1 import protobuf
-
-FIXTURES_DIR = join(dirname(abspath(__file__)), 'fixtures')
-ANCHOR_DRIVING_LICENSE = join(FIXTURES_DIR, 'anchor_driving_license.txt')
-ANCHOR_PASSPORT = join(FIXTURES_DIR, 'anchor_passport.txt')
-ANCHOR_YOTI_ADMIN = join(FIXTURES_DIR, 'anchor_yoti_admin.txt')
+from yoti_python_sdk import config
+from yoti_python_sdk.tests import anchor_parser
 
 
 def test_parse_anchors_driving_license():
-    parsed_anchor = parse_anchor_from_text(ANCHOR_DRIVING_LICENSE)
+    parsed_anchor = anchor_parser.get_driving_license_anchor()
 
     assert parsed_anchor.anchor_type == config.ANCHOR_SOURCE
     assert parsed_anchor.signed_timestamp == datetime.datetime(2018, 4, 11, 13, 13, 3, 923537)
@@ -23,7 +15,7 @@ def test_parse_anchors_driving_license():
 
 
 def test_parse_anchors_passport():
-    parsed_anchor = parse_anchor_from_text(ANCHOR_PASSPORT)
+    parsed_anchor = anchor_parser.get_passport_anchor()
 
     assert parsed_anchor.anchor_type == config.ANCHOR_SOURCE
     assert parsed_anchor.signed_timestamp == datetime.datetime(2018, 4, 12, 14, 14, 32, 835537)
@@ -32,25 +24,9 @@ def test_parse_anchors_passport():
 
 
 def test_parse_yoti_admin():
-    parsed_anchor = parse_anchor_from_text(ANCHOR_YOTI_ADMIN)
+    parsed_anchor = anchor_parser.get_yoti_admin_anchor()
 
     assert parsed_anchor.anchor_type == config.ANCHOR_VERIFIER
     assert parsed_anchor.signed_timestamp == datetime.datetime(2018, 4, 11, 13, 13, 4, 95238)
     assert parsed_anchor.sub_type == ""
     assert parsed_anchor.value == "YOTI_ADMIN"
-
-
-def parse_anchor_from_text(file_path):
-    base64_driving_license_anchor = read_file(file_path)
-    driving_license_anchor_bytes = binascii.a2b_base64(base64_driving_license_anchor)
-
-    protobuf_anchor = protobuf.Protobuf().anchor(driving_license_anchor_bytes)
-    anchors = list()
-    anchors.append(protobuf_anchor)
-
-    return anchor.Anchor().parse_anchors(anchors)[0]
-
-
-def read_file(file_path):
-    with io.open(file_path, mode='r', encoding='utf-8') as file:
-        return file.read()
