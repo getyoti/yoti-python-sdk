e174ef037df05081edcd10a47348a148026c17ba
Edward Harrod <echarrod@users.noreply.github.com>
2018-07-31 11:46:07 +0100

    SDK-350: Add source & verifiers filter

---
+++ yoti_python_sdk/tests/anchor_parser.py
@@ -0,0 +1,40 @@
+import binascii
+import io
+
+from os.path import abspath, dirname, join
+
+from yoti_python_sdk import anchor
+from yoti_python_sdk.protobuf.v1 import protobuf
+
+FIXTURES_DIR = join(dirname(abspath(__file__)), 'fixtures')
+ANCHOR_DRIVING_LICENSE = join(FIXTURES_DIR, 'anchor_driving_license.txt')
+ANCHOR_PASSPORT = join(FIXTURES_DIR, 'anchor_passport.txt')
+ANCHOR_YOTI_ADMIN = join(FIXTURES_DIR, 'anchor_yoti_admin.txt')
+
+
+def parse_anchor_from_base64_text(file_path):
+    base64_driving_license_anchor = read_file(file_path)
+    driving_license_anchor_bytes = binascii.a2b_base64(base64_driving_license_anchor)
+
+    protobuf_anchor = protobuf.Protobuf().anchor(driving_license_anchor_bytes)
+    anchors = list()
+    anchors.append(protobuf_anchor)
+
+    return anchor.Anchor().parse_anchors(anchors)[0]
+
+
+def get_driving_license_anchor():
+    return parse_anchor_from_base64_text(ANCHOR_DRIVING_LICENSE)
+
+
+def get_passport_anchor():
+    return parse_anchor_from_base64_text(ANCHOR_PASSPORT)
+
+
+def get_yoti_admin_anchor():
+    return parse_anchor_from_base64_text(ANCHOR_YOTI_ADMIN)
+
+
+def read_file(file_path):
+    with io.open(file_path, mode='r', encoding='utf-8') as file:
+        return file.read()
--- yoti_python_sdk/tests/test_attribute.py
+++ yoti_python_sdk/tests/test_attribute.py
@@ -1,13 +1,42 @@
 import yoti_python_sdk.attribute
 
+from yoti_python_sdk import config
+from yoti_python_sdk.tests import anchor_parser
+
+NAME = "name"
+VALUE = "value"
+
 
 def test_attribute_get_values():
-    name = "name"
-    value = "value"
     parsed_anchors = []
 
-    attribute = yoti_python_sdk.attribute.attribute(name, value, parsed_anchors)
+    attribute = yoti_python_sdk.attribute.attribute(NAME, VALUE, parsed_anchors)
 
-    assert attribute.get_name() == name
-    assert attribute.get_value() == value
+    assert attribute.get_name() == NAME
+    assert attribute.get_value() == VALUE
     assert attribute.get_anchors() == parsed_anchors
+
+
+def test_attribute_get_sources():
+    anchors = create_source_and_verifier_anchors()
+    attribute = yoti_python_sdk.attribute.attribute(NAME, VALUE, anchors)
+    sources = attribute.get_sources()
+
+    assert len(sources) == 1
+    assert sources[0].anchor_type == config.ANCHOR_SOURCE
+
+
+def test_attribute_get_verifiers():
+    anchors = create_source_and_verifier_anchors()
+    attribute = yoti_python_sdk.attribute.attribute(NAME, VALUE, anchors)
+    verifiers = attribute.get_verifiers()
+
+    assert len(verifiers) == 1
+    assert verifiers[0].anchor_type == config.ANCHOR_VERIFIER
+
+
+def create_source_and_verifier_anchors():
+    passport_anchor = anchor_parser.get_passport_anchor()  # source
+    yoti_admin_anchor = anchor_parser.get_yoti_admin_anchor()  # verifier
+
+    return [passport_anchor, yoti_admin_anchor]
