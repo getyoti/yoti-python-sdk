50e11de496d875316dadebfb2192364b4c5c774e
echarrod <echarrod@users.noreply.github.com>
2018-11-13 15:57:19 +0000

    [SDK-592]: Update cryptography package

--- yoti_python_sdk/tests/test_anchor.py
+++ yoti_python_sdk/tests/test_anchor.py
@@ -13,7 +13,7 @@ def test_parse_anchors_driving_license():
     assert parsed_anchor.signed_timestamp == datetime.datetime(2018, 4, 11, 13, 13, 3, 923537)
     assert parsed_anchor.sub_type == ""
     assert parsed_anchor.value == "DRIVING_LICENCE"
-    assert parsed_anchor.origin_server_certs.serial == int("46131813624213904216516051554755262812")
+    assert parsed_anchor.origin_server_certs.serial_number == int("46131813624213904216516051554755262812")
 
 
 def test_parse_anchors_passport():
@@ -23,7 +23,7 @@ def test_parse_anchors_passport():
     assert parsed_anchor.signed_timestamp == datetime.datetime(2018, 4, 12, 14, 14, 32, 835537)
     assert parsed_anchor.sub_type == "OCR"
     assert parsed_anchor.value == "PASSPORT"
-    assert parsed_anchor.origin_server_certs.serial == int("277870515583559162487099305254898397834")
+    assert parsed_anchor.origin_server_certs.serial_number == int("277870515583559162487099305254898397834")
 
 
 def test_parse_yoti_admin():
@@ -33,7 +33,7 @@ def test_parse_yoti_admin():
     assert parsed_anchor.signed_timestamp == datetime.datetime(2018, 4, 11, 13, 13, 4, 95238)
     assert parsed_anchor.sub_type == ""
     assert parsed_anchor.value == "YOTI_ADMIN"
-    assert parsed_anchor.origin_server_certs.serial == int("256616937783084706710155170893983549581")
+    assert parsed_anchor.origin_server_certs.serial_number == int("256616937783084706710155170893983549581")
 
 
 def test_anchor_returns_correct_default_values():
