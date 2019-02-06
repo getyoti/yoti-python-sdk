1c04fc7d0bb0deb266ee3c5743c9319cd53056a1
echarrod <echarrod@users.noreply.github.com>
2019-02-06 15:17:00 +0000

    SDK-668: Add version to request headers, extract version to separate file

--- yoti_python_sdk/__init__.py
+++ yoti_python_sdk/__init__.py
@@ -2,6 +2,7 @@
 from os import environ
 
 from yoti_python_sdk.client import Client
+from distutils.util import convert_path
 
 DEFAULTS = {
     'YOTI_API_URL': 'https://api.yoti.com',
@@ -9,6 +10,12 @@ DEFAULTS = {
     'YOTI_API_VERSION': 'v1',
 }
 
+main_ns = {}
+ver_path = convert_path('yoti_python_sdk/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+__version__ = main_ns['__version__']
 YOTI_API_URL = environ.get('YOTI_API_URL', DEFAULTS['YOTI_API_URL'])
 YOTI_API_PORT = environ.get('YOTI_API_PORT', DEFAULTS['YOTI_API_PORT'])
 YOTI_API_VERSION = environ.get('YOTI_API_VERSION', DEFAULTS['YOTI_API_VERSION'])
@@ -16,4 +23,5 @@ YOTI_API_ENDPOINT = '{0}:{1}/api/{2}'.format(YOTI_API_URL, YOTI_API_PORT, YOTI_A
 
 __all__ = [
     'Client',
+    __version__
 ]
