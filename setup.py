8e2398c580fd137c9f6df7e70e18dc2b3e828a23
echarrod <echarrod@users.noreply.github.com>
2019-02-11 08:52:01 -0700

    SDK-778: Update Django required version to prevent compatibility issue with Python 3.7+

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
     install_requires=['cryptography>=2.2.1', 'protobuf>=3.1.0',
                       'requests>=2.11.1', 'future>=0.11.0', 'asn1==2.2.0', 'pyopenssl>=18.0.0'],
     extras_require={
-        'examples': ['Django>=1.8', 'Flask>=0.10', 'python-dotenv>=0.7.1', 'django-sslserver>=0.2', 'Werkzeug==0.11.15'],
+        'examples': ['Django>1.11.16', 'Flask>=0.10', 'python-dotenv>=0.7.1', 'django-sslserver>=0.2',
+                     'Werkzeug==0.11.15'],
     },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
