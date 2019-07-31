98150a609f41935f325d9c32b0fda7d761d77c00
Emma Smith <Emma.Smith@Yoti.com>
2019-07-31 14:40:52 +0100

    SDK-1031: Add checking of timestamp and issuer to unknown anchor test
    The test for Unknown-anchor.txt in Ruby asserts the value of the
    timestamp and issuer. Replicating that functionality here.

--- yoti_python_sdk/tests/test_anchor.py
+++ yoti_python_sdk/tests/test_anchor.py
@@ -110,3 +110,12 @@ def test_processing_unknown_anchor_data():
     assert ("", "Unknown", "TEST UNKNOWN SUB TYPE") in [
         (anchor.value, anchor.anchor_type, anchor.sub_type) for anchor in anchors
     ]
+
+    expected_timestamp = datetime(2019, 3, 5, 10, 45, 11, 840037)
+    actual_timestamp = anchors[0].signed_timestamp
+
+    assert expected_timestamp == actual_timestamp
+
+    assert "document-registration-server" in [
+        a.value for a in anchors[0].origin_server_certs.issuer
+    ]
