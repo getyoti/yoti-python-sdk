05ccba436911a7c7cd924e02aa7348d557ca4360
Emma Smith <Emma.Smith@Yoti.com>
2019-08-08 11:13:48 +0100

    SDK-722: Remove inheritance dependency from TransactionalFlowExtensionBuilder

--- yoti_python_sdk/dynamic_sharing_service/extension/extension_builder.py
+++ yoti_python_sdk/dynamic_sharing_service/extension/extension_builder.py
@@ -6,25 +6,22 @@ class ExtensionBuilder(object):
     def __init__(self):
         self.__extension = {}
 
-    """
-    @param extension_type String label for the extension type
-    """
-
     def with_extension_type(self, extension_type):
+        """
+        @param extension_type String label for the extension type
+        """
         self.__extension["type"] = extension_type
         return self
 
-    """
-    @param content The extension content
-    """
-
     def with_content(self, content):
+        """
+        @param content The extension content
+        """
         self.__extension["content"] = content
         return self
 
-    """
-    @return Dictionary representation of an extension
-    """
-
     def build(self):
+        """
+        @return Dictionary representation of an extension
+        """
         return self.__extension.copy()
--- yoti_python_sdk/dynamic_sharing_service/extension/transactional_flow_extension_builder.py
+++ yoti_python_sdk/dynamic_sharing_service/extension/transactional_flow_extension_builder.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from .extension_builder import ExtensionBuilder
 
-
-class TransactionalFlowExtensionBuilder(ExtensionBuilder):
+class TransactionalFlowExtensionBuilder(object):
     TRANSACTIONAL_FLOW = "TRANSACTIONAL_FLOW"
 
     def __init__(self):
         super(TransactionalFlowExtensionBuilder, self).__init__()
         self.with_extension_type(self.TRANSACTIONAL_FLOW)
+
+    def with_content(self, content):
+        """
+        @param content The extension content
+        """
+        self.__extension["content"] = content
+
+    def build(self):
+        """
+        @return Dictionary representation of an extension
+        """
+        return self.__extension.copy()
