d0c2c381127bf48615b3c9ee00532dfbb01bca82
Alex Burt <alex.burt@yoti.com>
2019-08-23 17:10:24 +0100

    SDK-1076: Update client to use new signed request in get_activity_details

--- yoti_python_sdk/endpoint.py
+++ yoti_python_sdk/endpoint.py
@@ -1,3 +1,4 @@
+from deprecated import deprecated
 import time
 import uuid
 
@@ -6,7 +7,14 @@ class Endpoint(object):
     def __init__(self, sdk_id):
         self.sdk_id = sdk_id
 
-    def get_activity_details_request_path(self, decrypted_request_token):
+    def get_activity_details_request_path(
+        self, decrypted_request_token, no_params=False
+    ):
+        # TODO: Remove no_params in 3.0.0 and get rid of the query parameters in the formatted
+        # TODO: string
+        if no_params:
+            return "/profile/{0}".format(decrypted_request_token)
+
         return "/profile/{0}?nonce={1}&timestamp={2}&appId={3}".format(
             decrypted_request_token,
             self.__create_nonce(),
@@ -14,12 +22,18 @@ class Endpoint(object):
             self.sdk_id,
         )
 
-    def get_aml_request_url(self):
+    def get_aml_request_url(self, no_params=False):
+        if no_params:
+            return "/aml-check"
+
         return "/aml-check?appId={0}&timestamp={1}&nonce={2}".format(
             self.sdk_id, self.__create_timestamp(), self.__create_nonce()
         )
 
-    def get_dynamic_share_request_url(self):
+    def get_dynamic_share_request_url(self, no_params=False):
+        if no_params:
+            return "/qrcodes/app/{appid}".format(appid=self.sdk_id)
+
         return "/qrcodes/apps/{appid}?nonce={nonce}&timestamp={timestamp}".format(
             appid=self.sdk_id,
             nonce=self.__create_nonce(),
@@ -27,9 +41,11 @@ class Endpoint(object):
         )
 
     @staticmethod
+    @deprecated
     def __create_nonce():
         return uuid.uuid4()
 
     @staticmethod
+    @deprecated
     def __create_timestamp():
         return int(time.time() * 1000)
