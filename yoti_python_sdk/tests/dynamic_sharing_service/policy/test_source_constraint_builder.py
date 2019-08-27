e199763592ec874336968d757f77b34c686ce97b
Emma Smith <Emma.Smith@Yoti.com>
2019-08-27 16:19:01 +0100

    SDK-1123: Rename anchor constants to ANCHOR_VALUE_X

--- yoti_python_sdk/tests/dynamic_sharing_service/policy/test_source_constraint_builder.py
+++ yoti_python_sdk/tests/dynamic_sharing_service/policy/test_source_constraint_builder.py
@@ -1,7 +1,7 @@
 from yoti_python_sdk.dynamic_sharing_service.policy.source_constraint_builder import (
     SourceConstraintBuilder,
 )
-from yoti_python_sdk.config import DRIVING_LICENCE, PASSPORT
+from yoti_python_sdk.config import ANCHOR_VALUE_DRIVING_LICENCE, ANCHOR_VALUE_PASSPORT
 
 
 def test_build():
@@ -17,7 +17,7 @@ def test_with_driving_licence():
 
     anchors = constraint["preferred_sources"]["anchors"]
     assert len(anchors) == 1
-    assert DRIVING_LICENCE in [a["name"] for a in anchors]
+    assert ANCHOR_VALUE_DRIVING_LICENCE in [a["name"] for a in anchors]
 
 
 def test_with_soft_preference():
@@ -30,6 +30,6 @@ def test_with_soft_preference():
     )
     anchors = constraint["preferred_sources"]["anchors"]
     assert len(anchors) == 2
-    assert DRIVING_LICENCE in [a["name"] for a in anchors]
-    assert PASSPORT in [a["name"] for a in anchors]
+    assert ANCHOR_VALUE_DRIVING_LICENCE in [a["name"] for a in anchors]
+    assert ANCHOR_VALUE_PASSPORT in [a["name"] for a in anchors]
     assert constraint["preferred_sources"]["soft_preference"]
