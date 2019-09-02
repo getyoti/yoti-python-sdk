d9f4103e375440055d9ab1c0cf2000b89a8eb13d
Alex Burt <alex.burt@yoti.com>
2019-09-02 12:11:35 +0100

    SDK-573: Update Django example project to use AgeVerification object

--- examples/yoti_example_django/yoti_example/views.py
+++ examples/yoti_example_django/yoti_example/views.py
@@ -71,10 +71,18 @@ class AuthView(TemplateView):
         context["receipt_id"] = getattr(activity_details, "receipt_id")
         context["timestamp"] = getattr(activity_details, "timestamp")
 
-        # change this string according to the age condition defined in Yoti Hub
-        age_verified = profile.get_attribute("age_over:18")
+        # change this number according to the age condition defined in Yoti Hub
+        age_verified = profile.find_age_over_verification(18)
+
+        # Age verification objects don't have the same properties as an attribute,
+        # so for this example we had to mock an object with the same properties
         if age_verified is not None:
-            context["age_verified"] = age_verified
+            context["age_verified"] = {
+                "name": "age_verified",
+                "value": age_verified,
+                "sources": age_verified.attribute.sources,
+                "verifiers": age_verified.attribute.verifiers,
+            }
 
         selfie = context.get("selfie")
         if selfie is not None:
