9a39418cccdd71e77ced6a73b66d9c720760dc62
Alex Burt <alex.burt@yoti.com>
2019-09-10 10:30:09 +0100

    SDK-1076: Move create_nonce and create_timestamp into a utils module, and update references in the rest of the SDK

--- yoti_python_sdk/sandbox/endpoint.py
+++ yoti_python_sdk/sandbox/endpoint.py
@@ -1,6 +1,5 @@
 from yoti_python_sdk.endpoint import Endpoint
-import time
-import uuid
+from yoti_python_sdk.utils import create_timestamp, create_nonce
 
 
 class SandboxEndpoint(Endpoint):
@@ -9,15 +8,5 @@ class SandboxEndpoint(Endpoint):
 
     def get_sandbox_path(self):
         return "/apps/{sdk_id}/tokens?timestamp={timestamp}&nonce={nonce}".format(
-            sdk_id=self.sdk_id,
-            nonce=self.__create_nonce(),
-            timestamp=self.__create_timestamp(),
+            sdk_id=self.sdk_id, nonce=create_nonce(), timestamp=create_timestamp()
         )
-
-    @staticmethod
-    def __create_nonce():
-        return uuid.uuid4()
-
-    @staticmethod
-    def __create_timestamp():
-        return int(time.time() * 1000)
