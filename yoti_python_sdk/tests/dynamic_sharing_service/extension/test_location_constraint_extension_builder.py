2cacd346b1af954e3012a2ddb81238e8cb7cba8d
Emma Smith <Emma.Smith@Yoti.com>
2019-09-23 10:39:34 +0100

    SDK-1196: Correct mispelling of longitude in Python SDK

--- yoti_python_sdk/tests/dynamic_sharing_service/extension/test_location_constraint_extension_builder.py
+++ yoti_python_sdk/tests/dynamic_sharing_service/extension/test_location_constraint_extension_builder.py
@@ -8,14 +8,14 @@ from yoti_python_sdk.dynamic_sharing_service.extension.location_constraint_exten
 
 def test_builds_with_given_values():
     LATITUDE = 50
-    LONGTITUDE = 99
+    LONGITUDE = 99
     RADIUS = 60
     UNCERTAINTY = 30
 
     extension = (
         LocationConstraintExtensionBuilder()
         .with_latitude(LATITUDE)
-        .with_longtitude(LONGTITUDE)
+        .with_longitude(LONGITUDE)
         .with_radius(RADIUS)
         .with_uncertainty(UNCERTAINTY)
         .build()
@@ -24,6 +24,6 @@ def test_builds_with_given_values():
     device_location = extension["content"]["expected_device_location"]
 
     assert device_location["latitude"] == LATITUDE
-    assert device_location["longtitude"] == LONGTITUDE
+    assert device_location["longitude"] == LONGITUDE
     assert device_location["radius"] == RADIUS
     assert device_location["max_uncertainty_radius"] == UNCERTAINTY
