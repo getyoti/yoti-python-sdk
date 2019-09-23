c60b4945d7f6081c8b45de90dc07185c19a2ceb9
Emma Smith <Emma.Smith@Yoti.com>
2019-09-23 11:42:36 +0100

    SDK-1196: Copy device location values validation from Java implementation

--- yoti_python_sdk/dynamic_sharing_service/extension/location_constraint_extension_builder.py
+++ yoti_python_sdk/dynamic_sharing_service/extension/location_constraint_extension_builder.py
@@ -20,6 +20,10 @@ class LocationConstraintExtensionBuilder(object):
         self.__extension["content"]["expected_device_location"] = self.__device_location
 
     def with_latitude(self, latitude):
+        if not isinstance(latitude, float) and not isinstance(latitude, int):
+            raise ValueError("Latitude must be float or int")
+        if not -90 <= latitude <= 90:
+            raise ValueError("Latitude must be between -90 and 90 degrees")
         self.__device_location["latitude"] = latitude
         return self
 
@@ -32,14 +36,26 @@ class LocationConstraintExtensionBuilder(object):
         return self.with_longitude(longtitude)
 
     def with_longitude(self, longitude):
+        if not isinstance(longitude, float) and not isinstance(longitude, int):
+            raise ValueError("Lontitude must be float or int")
+        if not -180 <= longitude <= 180:
+            raise ValueError("Longitude must be between -180 and 180 degrees")
         self.__device_location["longitude"] = longitude
         return self
 
     def with_radius(self, radius):
+        if not isinstance(radius, float) and not isinstance(radius, int):
+            raise ValueError("Radius must be float or int")
+        if not 0 <= radius:
+            raise ValueError("Radius must be >= 0")
         self.__device_location["radius"] = radius
         return self
 
     def with_uncertainty(self, uncertainty):
+        if not isinstance(uncertainty, float) and not isinstance(uncertainty, int):
+            raise ValueError("Uncertainty must be float or int")
+        if not 0 <= uncertainty:
+            raise ValueError("Uncertainty must be >= 0")
         self.__device_location["max_uncertainty_radius"] = uncertainty
         return self
 
--- yoti_python_sdk/tests/dynamic_sharing_service/extension/test_location_constraint_extension_builder.py
+++ yoti_python_sdk/tests/dynamic_sharing_service/extension/test_location_constraint_extension_builder.py
@@ -4,6 +4,61 @@ from __future__ import unicode_literals
 from yoti_python_sdk.dynamic_sharing_service.extension.location_constraint_extension_builder import (
     LocationConstraintExtensionBuilder,
 )
+import pytest
+
+
+def test_longitude_validation():
+    extension = LocationConstraintExtensionBuilder()
+    with pytest.raises(ValueError):
+        extension.with_longitude(270.00)
+    with pytest.raises(ValueError):
+        extension.with_longitude(-181)
+    with pytest.raises(ValueError):
+        extension.with_longitude("27.00")
+    extension.with_longitude(180.0)
+    extension.with_longitude(-90)
+
+
+def test_latitude_validation():
+    extension = LocationConstraintExtensionBuilder()
+    with pytest.raises(ValueError):
+        extension.with_latitude(270.00)
+    with pytest.raises(ValueError):
+        extension.with_latitude(-181)
+    with pytest.raises(ValueError):
+        extension.with_latitude("27.00")
+    with pytest.raises(ValueError):
+        extension.with_latitude(91)
+    with pytest.raises(ValueError):
+        extension.with_latitude(-180)
+    extension.with_latitude(18.0)
+    extension.with_latitude(-90)
+
+
+def test_uncertainty_validation():
+    extension = LocationConstraintExtensionBuilder()
+    with pytest.raises(ValueError):
+        extension.with_uncertainty(-1)
+    with pytest.raises(ValueError):
+        extension.with_uncertainty(-0.01)
+    with pytest.raises(ValueError):
+        extension.with_uncertainty("3")
+    extension.with_uncertainty(0)
+    extension.with_uncertainty(1)
+    extension.with_uncertainty(1e3)
+
+
+def test_radius_validation():
+    extension = LocationConstraintExtensionBuilder()
+    with pytest.raises(ValueError):
+        extension.with_radius(-1)
+    with pytest.raises(ValueError):
+        extension.with_radius(-0.01)
+    with pytest.raises(ValueError):
+        extension.with_radius("3")
+    extension.with_radius(0)
+    extension.with_radius(1)
+    extension.with_radius(1e3)
 
 
 def test_builds_with_given_values():
