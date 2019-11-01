41441153a251496ae12afe383df828298d44cd9c
Emma Smith <Emma.Smith@Yoti.com>
2019-11-01 17:05:02 +0000

    SDK-1229: Remove SimpleNamespace fixtures
    Use Protobuf objects for all testing in test_extra_data, without relying
    on SimpleNamespace mocks.

--- yoti_python_sdk/share/extra_data.py
+++ yoti_python_sdk/share/extra_data.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from yoti_python_sdk.issuance_details import IssuanceDetails
+from yoti_python_sdk.protobuf.share_public_api import ThirdPartyAttribute_pb2
 
 
 class ExtraData(object):
     THIRD_PARTY_ATTRIBUTE = 6
 
-    def __init__(self, data_entries_list):
+    def __init__(self, proto):
         self.__attribute_issuance_details = None
+        data_entries_list = proto.list
 
         for data_entry in data_entries_list:
             if (
                 data_entry.type == self.THIRD_PARTY_ATTRIBUTE
                 and self.__attribute_issuance_details is None
             ):
-                self.__attribute_issuance_details = IssuanceDetails(data_entry)
+                attribute = ThirdPartyAttribute_pb2.ThirdPartyAttribute()
+                attribute.MergeFromString(data_entry.value)
+                self.__attribute_issuance_details = IssuanceDetails(attribute)
 
     @property
     def attribute_issuance_details(self):
