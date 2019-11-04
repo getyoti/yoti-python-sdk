3a7f79ef47e47975dc9788ca28ef7c80c69a8190
Emma Smith <Emma.Smith@Yoti.com>
2019-11-04 14:44:05 +0000

    SDK-1229: Implement test for invalid dates and move date parser

--- yoti_python_sdk/issuance_details.py
+++ yoti_python_sdk/issuance_details.py
@@ -1,32 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
-import re
+from yoti_python_sdk import date_parser
 
 
 class IssuanceDetails(object):
     def __init__(self, data_entry):
         self.__token = data_entry.issuance_token.decode()
-        if (
-            data_entry.issuing_attributes.expiry_date != ""
-            and data_entry.issuing_attributes.expiry_date is not None
-        ):
-            time_split = re.split("[^0-9]", data_entry.issuing_attributes.expiry_date)
-            print("!DEBUG! %s" % time_split)
-            assert len(time_split) >= 7
-            year = int(time_split[0])
-            month = int(time_split[1])
-            day = int(time_split[2])
-            hour = int(time_split[3])
-            minute = int(time_split[4])
-            second = int(time_split[5])
-            microsecond = int(float("0." + time_split[6]) * 1e6)
-            self.__expiry_date = datetime(
-                year, month, day, hour, minute, second, microsecond
-            )
-            # TODO: Refactor time parser into own method
-        else:
-            self.__expiry_date = None
+        self.__expiry_date = date_parser.datetime_with_microsecond(
+            data_entry.issuing_attributes.expiry_date
+        )
         self.__attributes = data_entry.issuing_attributes.definitions
 
     @property
