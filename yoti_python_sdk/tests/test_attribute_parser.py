37325e28a69298add6afe108311fe46a0f0e5337
Alex Burt <alex.burt@yoti.com>
2019-11-22 15:33:47 +0000

    SDK-681: Update attribute parser to now store date attributes as datetime.date

--- yoti_python_sdk/tests/test_attribute_parser.py
+++ yoti_python_sdk/tests/test_attribute_parser.py
@@ -3,26 +3,30 @@ import logging
 
 import pytest
 
+from datetime import date
+
 from yoti_python_sdk import attribute_parser
-from yoti_python_sdk.protobuf import protobuf
+from yoti_python_sdk.protobuf.protobuf import Protobuf
 
 STRING_VALUE = "123"
 BYTE_VALUE = str.encode(STRING_VALUE)
 INT_VALUE = int(STRING_VALUE)
 
+DATE_VALUE = "1995-04-20"
+DATE_BYTE_VALUE = str.encode(DATE_VALUE)
+
+INCORRECT_DATE_VALUE = "95-222-10"
+INCORRECT_DATE_BYTE_VALUE = str.encode(INCORRECT_DATE_VALUE)
+
 
 @pytest.fixture(scope="module")
 def proto():
-    return protobuf.Protobuf()
+    return Protobuf()
 
 
 @pytest.mark.parametrize(
     "content_type, expected_value",
-    [
-        (protobuf.Protobuf.CT_STRING, STRING_VALUE),
-        (protobuf.Protobuf.CT_DATE, STRING_VALUE),
-        (protobuf.Protobuf.CT_INT, INT_VALUE),
-    ],
+    [(Protobuf.CT_STRING, STRING_VALUE), (Protobuf.CT_INT, INT_VALUE)],
 )
 def test_attribute_parser_values_based_on_content_type(content_type, expected_value):
     result = attribute_parser.value_based_on_content_type(BYTE_VALUE, content_type)
@@ -58,3 +62,21 @@ def test_jpeg_image_value_based_on_content_type(proto):
     result = attribute_parser.value_based_on_content_type(BYTE_VALUE, proto.CT_JPEG)
     assert result.data == BYTE_VALUE
     assert result.content_type == "jpeg"
+
+
+def test_date_proto_should_parse_into_date_object():
+    result = attribute_parser.value_based_on_content_type(
+        DATE_BYTE_VALUE, Protobuf.CT_DATE
+    )
+    assert isinstance(result, date)
+    assert result.strftime("%Y-%m-%d") == DATE_VALUE
+    assert result.year == 1995
+    assert result.month == 4
+    assert result.day == 20
+
+
+def test_should_raise_value_error_on_incorrect_format_date():
+    with pytest.raises(ValueError):
+        attribute_parser.value_based_on_content_type(
+            INCORRECT_DATE_BYTE_VALUE, Protobuf.CT_DATE
+        )
