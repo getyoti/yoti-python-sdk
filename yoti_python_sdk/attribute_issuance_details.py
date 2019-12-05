eb16429af0432a832c133799649672b4d0680abf
Emma Smith <Emma.Smith@Yoti.com>
2019-12-05 11:46:28 +0000

    SDK-1229: Convert IssuingAttributes_pb2.definitions to Python list

--- yoti_python_sdk/attribute_issuance_details.py
+++ yoti_python_sdk/attribute_issuance_details.py
@@ -4,13 +4,24 @@ from yoti_python_sdk import date_parser
 import base64
 
 
+class Definition(object):
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+
 class AttributeIssuanceDetails(object):
     def __init__(self, data_entry):
         self.__token = base64.b64encode(data_entry.issuance_token)
         self.__expiry_date = date_parser.datetime_with_microsecond(
             data_entry.issuing_attributes.expiry_date
         )
-        self.__attributes = data_entry.issuing_attributes.definitions
+        self.__attributes = [
+            Definition(a.name) for a in data_entry.issuing_attributes.definitions
+        ]
 
     @property
     def token(self):
