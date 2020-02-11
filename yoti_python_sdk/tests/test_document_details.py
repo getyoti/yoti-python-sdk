e11f8470c3296476e739ca920f120c1c74257223
Emma Smith <Emma.Smith@Yoti.com>
2020-02-11 17:00:38 +0000

    SDK-1443: Remove regex validation from document details
    Support for parsing redacted aadhaar numbers

--- yoti_python_sdk/tests/test_document_details.py
+++ yoti_python_sdk/tests/test_document_details.py
@@ -34,6 +34,40 @@ def test_parse_3_words():
     assert document.issuing_authority is None
 
 
+def test_parse_redacted_aadhaar():
+    DATA = "AADHAAR IND ****1234"
+
+    document = DocumentDetails(DATA)
+
+    assert document.document_type == "AADHAAR"
+    assert document.issuing_country == "IND"
+    assert document.document_number == "****1234"
+    assert document.expiration_date is None
+    assert document.issuing_authority is None
+
+
+@pytest.mark.parametrize(
+    "details, expected_number",
+    [
+        ("type country **** - authority", "****"),
+        (
+            "type country ~!@#$%^&*()-_=+[]{}|;':,./<>? - authority",
+            "~!@#$%^&*()-_=+[]{}|;':,./<>?",
+        ),
+        ('type country "" - authority', '""'),
+        ("type country \\ - authority", "\\"),
+        ('type country " - authority', '"'),
+        ("type country '' - authority", "''"),
+        ("type country ' - authority", "'"),
+    ],
+)
+def test_parse_special_characters(details, expected_number):
+    document = DocumentDetails(details)
+
+    assert document.document_type == "type"
+    assert document.document_number == expected_number
+
+
 def test_parse_4_words():
     DATA = "DRIVING_LICENCE GBR 1234abc 2016-05-01"
 
@@ -88,3 +122,12 @@ def test_invalid_date():
     with pytest.raises(ValueError) as exc:
         DocumentDetails(DATA)
         assert str(exc.value) == "Invalid value for DocumentDetails"
+
+
+def test_should_parse_with_double_space():
+    DATA = "AADHAAR  IND ****1234"
+
+    document = DocumentDetails(DATA)
+
+    assert document.document_type == "AADHAAR"
+    assert document.document_number == "****1234"
