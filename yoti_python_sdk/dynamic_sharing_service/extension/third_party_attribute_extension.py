37b1e02622324e0b3e0e9bbbee8159a3cca3e88d
echarrod <echarrod@users.noreply.github.com>
2020-06-08 14:10:13 +0100

    SDK-1589: Update Attibute Issuance Expiry Date format to be milliseconds, not microseconds; update version

--- yoti_python_sdk/dynamic_sharing_service/extension/third_party_attribute_extension.py
+++ yoti_python_sdk/dynamic_sharing_service/extension/third_party_attribute_extension.py
@@ -1,23 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 import copy
+
+import pytz
 
 
 class ThirdPartyAttributeExtension(object):
     THIRDPARTY_ATTRIBUTE = "THIRD_PARTY_ATTRIBUTE"
 
     def __init__(self):
-        self.__extension = {}
-        self.__extension["type"] = self.THIRDPARTY_ATTRIBUTE
-        self.__extension["content"] = {"expiry_date": None, "definitions": []}
+        self.__extension = {
+            "type": self.THIRDPARTY_ATTRIBUTE,
+            "content": {"expiry_date": None, "definitions": []},
+        }
 
     def with_expiry_date(self, expiry_date):
-        self.__extension["content"]["expiry_date"] = expiry_date.isoformat()
+        """
+        :param expiry_date: Expiry date for the attribute. If no timezone info is provided, UTC will be used.
+        :type expiry_date: datetime
+        """
+        if expiry_date.tzinfo is None:
+            expiry_date = expiry_date.replace(tzinfo=pytz.UTC)
+
+        utc_time = expiry_date.astimezone(pytz.utc)
+        rfc_3339_milliseconds = utc_time.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+        self.__extension["content"]["expiry_date"] = rfc_3339_milliseconds + "Z"
         return self
 
     def with_definitions(self, *names):
+        """
+        :param names: attribute definitions
+        :type names: str or list[str]
+        """
         self.__extension["content"]["definitions"].extend([{"name": s} for s in names])
         return self
 
     def build(self):
+        """
+        Builds the object
+
+        :return: the third party attribute
+        :rtype: ThirdPartyAttributeExtension
+        """
         return copy.deepcopy(self.__extension)
