810c1d415e1327f327b01b3e93f9390a39aedbb1
David Grayston <davidgrayston@googlemail.com>
2020-11-09 13:43:39 +0000

    SDK-1615: Export ThirdPartyAttributeExtension

--- yoti_python_sdk/dynamic_sharing_service/extension/__init__.py
+++ yoti_python_sdk/dynamic_sharing_service/extension/__init__.py
@@ -1,9 +1,11 @@
 from .extension_builder import ExtensionBuilder
 from .location_constraint_extension_builder import LocationConstraintExtensionBuilder
 from .transactional_flow_extension_builder import TransactionalFlowExtensionBuilder
+from .third_party_attribute_extension import ThirdPartyAttributeExtension
 
 __all__ = [
     "ExtensionBuilder",
     "LocationConstraintExtensionBuilder",
     "TransactionalFlowExtensionBuilder",
+    "ThirdPartyAttributeExtension",
 ]
