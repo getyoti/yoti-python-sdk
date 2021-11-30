07bb943d9056cd0f5b07eb7a7e86e4dc96467891
Michal Milewicz <michal.milewicz@yoti.com>
2021-11-30 09:44:50 +0100

    [SDK-2004] Add support for issuing authority sub check

---
+++ yoti_python_sdk/tests/doc_scan/session/create/subcheck/test_issuing_authority_sub_check.py
@@ -0,0 +1,41 @@
+import unittest
+
+from yoti_python_sdk.doc_scan.session.create.filter.orthogonal_restrictions_filter import \
+    OrthogonalRestrictionsFilterBuilder
+from yoti_python_sdk.doc_scan.session.create.subcheck import (
+    IssuingAuthoritySubCheckBuilder
+)
+from yoti_python_sdk.doc_scan.session.create.subcheck.issuing_authority_sub_check import IssuingAuthoritySubCheck
+
+
+class IssuingAuthoritySubCheckTest(unittest.TestCase):
+    def test_should_build_correctly_without_additional_data(self):
+        issuing_authority_sub_check = IssuingAuthoritySubCheckBuilder().build()
+
+        assert isinstance(issuing_authority_sub_check, IssuingAuthoritySubCheck)
+
+    def test_should_build_correctly_with_filter(self):
+        filter = OrthogonalRestrictionsFilterBuilder().with_whitelisted_country_codes(
+            ["GBR", "FRA"]).with_whitelisted_document_types(["PASSPORT", "STATE_ID"]).build()
+
+        issuing_authority_sub_check = IssuingAuthoritySubCheckBuilder().with_filter(
+            filter=filter).build()
+
+        assert isinstance(issuing_authority_sub_check, IssuingAuthoritySubCheck)
+        assert issuing_authority_sub_check.filter == filter
+
+    def test_should_always_build_with_requested_as_boolean_true(self):
+        issuing_authority_sub_check = IssuingAuthoritySubCheckBuilder().build()
+
+        assert issuing_authority_sub_check.requested is True
+
+    def test_build_invalid_filter(self):
+        filter = 'invalid'
+
+        with self.assertRaises(ValueError):
+            IssuingAuthoritySubCheckBuilder().with_filter(
+                filter=filter).build()
+
+
+if __name__ == "__main__":
+    unittest.main()
