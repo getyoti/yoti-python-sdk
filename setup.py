5efb10af00df41a3ac85efedd033968510b14a8c
mehmet-yoti <mehmet.sepici@yoti.com>
2025-07-31 12:41:50 +0100

    SDK-2648 updated version of protobuf

--- setup.py
+++ setup.py
@@ -17,12 +17,13 @@ setup(
     url="https://github.com/getyoti/yoti-python-sdk",
     author="Yoti",
     author_email="websdk@yoti.com",
+    python_requires=">=3.9",
     install_requires=[
 
 
         "asn1==2.2.0",  # still pinned due to enum34 issue
         "cryptography>=42.0.0",
-        "protobuf==3.20.3",
+        "protobuf==4.21.12",
         "requests>=2.31.0",
         "pyopenssl>=24.0.0",
         "pytz>=2025.2",
@@ -58,7 +59,6 @@ setup(
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
